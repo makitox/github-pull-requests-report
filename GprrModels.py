86f5fdc6ebfc3299ef127d45436abae81b6003fd
Ruslan Telenkevich <ruslantelenkevich@gmail.com>
2020-06-12 01:11:16 +0300

    continue refactoring. Update all models, working on GPRR routins

--- GprrModels.py
+++ GprrModels.py
@@ -1,74 +1,169 @@
-from typing import List, Dict
+from datetime import datetime
+from typing import List, Dict, Set, Generic, TypeVar
 
 
 class GprrUser(object):
     """
     User representation in GPRR
     """
-    login: str = None
-    name: str = None
-    url: str = None
+
+    def __init__(
+            self,
+            login: str = None,
+            name: str = None,
+            url: str = None,
+            id: int = None,
+    ):
+        assert login is not None
+        assert name is not None
+        assert url is not None
+        assert login is not None
+
+        self.login = login
+        self.name = name
+        self.id = id
+        self.url = url
+
 
 class GprrReview(object):
     """
     Review representation in GPRR
     """
-    user: GprrUser = None
-    state: str = None
-    submitted_at: str = None
-    url: str = None
+
+    def __init(
+            self,
+            user: GprrUser,
+            state: str,
+            submitted_at: datetime = None,
+            url: str = None,
+    ):
+        assert user is not None
+        assert state is not None
+
+        self.user = user
+        self.state = state
+        self.submitted_at = submitted_at
+        self.url = url
+
 
 class GprrPrFlag(object):
     """ Representation of GitHub flag, used in GPRR"""
-    flag: str = None
-    value: str = None
+
+    def __init__(
+            self,
+            flag: str = None,
+            value: str = None
+    ):
+        assert flag is not None
+        assert value is not None
+        self.flag = flag
+        self.value = value
+
 
 class GprrPrLabel(object):
     """ Representation of GitHub label, used in GPRR"""
-    title: str = None
-    color: str = None
+
+    def __init__(
+            self,
+            title: str = None,
+            color: str = None,
+            description: str = None
+    ):
+        assert title is not None
+        assert color is not None
+        assert description is not None
+        self.title = title
+        self.color = color
+        self.description = description
+
 
 class GprrPR(object):
     """ Representation of pull request, used in GPRR"""
-    id: int = None
-    number: int = None
-    url: str = None
-    repository: str = None
-    title: str = None
-    flags: List[GprrPrFlag] = []
-    labels: List[GprrPrLabel] = []
-    creator: GprrUser = None
-    reviewers: List[GprrReview] = []
-    assignees: List[GprrUser] = []
-    created: str = None
-    updated: str = None
-    since_updated: int = None
-    initial_branch: str = None
-    initial_branch_url: str = None
+
+    def __init__(self):
+        self.id: int = None
+        self.number: int = None
+        self.url: str = None
+        self.repository: str = None
+        self.title: str = None
+        self.flags: List[GprrPrFlag] = []
+        self.labels: List[GprrPrLabel] = []
+        self.creator: GprrUser = None
+        self.reviews: List[GprrReview] = []
+        self.assignees: List[GprrUser] = []
+        self.created: datetime = None
+        self.updated: datetime = None
+        self.since_updated: int = None
+        self.initial_branch: str = None
+        self.initial_branch_url: str = None
+
+
+class GprrRepository(object):
+
+    def __init__(
+            self,
+            id: int,
+            title: str,
+            url: str,
+            name: str,
+    ):
+        assert id is not None
+        assert title is not None
+        assert url is not None
+        assert name is not None
+        self.id = id
+        self.title = title
+        self.url = url
+        self.name = name
+
 
 
 class PrContainer(object):
-    title: str = None
-    sections: Dict[str, List[GprrPR]]
 
-    def initSection(
+    def __init__(
             self,
-            section_name: str = "",
-            section_elements: List[GprrPR] = []
+            title: str = ""
     ):
-        self.sections[section_name] = section_elements
+        self.title = title
+        self.container: Dict[str, List[GprrPR]] = {}
 
-    def appendSection(
+    def append_item(
             self,
-            section_name: str = "",
-            section_elements: List[GprrPR] = []
+            item: GprrPR,
+            item_group: str = ""
     ):
-        if section_name in self.sections.keys():
-            _list = self.sections[section_name]
-            _list.extend(section_elements)
-            self.sections[section_name] = _list
+        assert item is not None
+
+        if item_group in self.container.keys():
+            self.container[item_group].add(item)
         else:
-            self.sections[section_name] = section_elements
+            self.container[item_group] = [item]
 
 
+T = TypeVar('T')
 
+
+class Filter(Generic[T]):
+
+    def __init__(
+            self,
+            title: str
+    ):
+        assert title is not None
+        self.title = title
+        self.items: [T] = []
+
+    def contains(self, item: T) -> bool:
+        return self.contains_item_id(item.id)
+
+    def add(self, item: T):
+        assert item is not None
+        if not self.contains_item_id(item.id):
+            self.items.add(item)
+
+    def contains_item_id(self, item_id: int) -> bool:
+        assert item_id is not None
+        for usr in self.items:
+            if usr.id == item_id:
+                return True
+        return False
--- GprrRoutins.py
+++ GprrRoutins.py
@@ -2,45 +2,187 @@ from typing import List, Set
 import datetime
 
 from github import Github
+from github.Label import Label
+from github.NamedUser import NamedUser
+from github.PullRequest import PullRequest
+from github.Repository import Repository
 
-from GprrModels import PrContainer
+from GprrModels import PrContainer, AccountsFilter, RepositoriesFilter, GprrUser, GprrPR, GprrPrFlag, GprrPrLabel, \
+    GprrReview, Filter, GprrRepository
 from globals import CONFIGURATION, APPCONFIG_SECTION, HTML_REPORT_FILE_NAME, JSON_REPORT_FILE_NAME
 
 
 class GprrReport(object):
 
-    __default_html_report_name: str = datetime.datetime.today().strftime(CONFIGURATION[APPCONFIG_SECTION][HTML_REPORT_FILE_NAME])
-    __default_json_report_name: str = datetime.datetime.today().strftime(CONFIGURATION[APPCONFIG_SECTION][JSON_REPORT_FILE_NAME])
-    __github: Github = None
-
-    fullList: PrContainer = None
-    byRepos: PrContainer = None
-    byAuthor: PrContainer = None
-    byAssignee: PrContainer = None
-    byReviewer: PrContainer = None
-
     def __init__(
             self,
-            token: str = None
+            token: str = None,
+            logins: Set[str] = None,
+            repos: Set[str] = None,
+            org:str = None,
+            per_page: int = 300
     ):
-        self.__github = Github(token, )
+        assert logins is not None
+        assert repos is not None
+        assert org is not None
 
-    def collect_data(
-            self,
-            companies: Set = {},
-            repositories: Set = {},
-            logins: Set = {}
-    ):
-        pass
+        self.default_html_report_name = datetime.datetime.today().strftime(CONFIGURATION[APPCONFIG_SECTION][HTML_REPORT_FILE_NAME])
+        self.default_json_report_name = datetime.datetime.today().strftime(CONFIGURATION[APPCONFIG_SECTION][JSON_REPORT_FILE_NAME])
+        self.github = Github(token, per_page=per_page)
+        self.organization_str = org
 
-    def generate_html_report(
-            self,
-            filename: str = __default_html_report_name
-    ):
-        pass
+        self.accounts_filter: Filter[GprrUser] = Filter("account filter")
+        self.repo_filter: Filter[GprrRepository] = Filter("repository filter")
 
-    def generate_json_report(
+        self.full_list = PrContainer("Full list")
+        self.by_author = PrContainer("By author")
+        self.by_reviewer = PrContainer("By reviewer")
+        self.by_repository = PrContainer("By repository")
+        self.by_assignee = PrContainer("By assignee")
+
+
+    def collect_data(self, repositories: Set = {}, logins: Set = {}):
+        organization = self.github.get_organization(self.organization_str)
+        if len(logins) == 0:  # no filter, add all logins
+            for usr in organization.get_members():
+                gprr_user = GprrUser(id=usr.id, login=usr.login, name=usr.name, url=usr.html_url)
+                self.accounts_filter.add(gprr_user)
+        else: # use logins from defined list
+            for login in logins:
+                usr = self.github.get_user(login)
+                gprr_user = GprrUser(id=usr.id, login=login, name=usr.name, url=usr.html_url)
+                self.accounts_filter.add(gprr_user)
+
+        if len(repositories) == 0:
+            for repo in organization.get_repos(type='all'):
+                self.__process_repo(repo)
+        else:
+            for repo_str in repositories:
+                repo = self.github.get_repo(repo_str)
+                self.__process_repo(repo)
+
+    def __process_repo(self, repo: Repository):
+        gprr_repo = self.__convert_repo(repo)
+        self.repo_filter.add(gprr_repo)
+        self.__process_all_prs(repo)
+
+    def __process_all_prs(self, repo: Repository):
+        for pr in repo.get_pulls(state='open'):
+            marked_pr = False
+            gprr_pr = self.__convert_pr(pr)
+
+            if self.accounts_filter.contains_item_id(gprr_pr.creator.id):
+                self.by_author.append_item(gprr_pr, gprr_pr.creator.name)
+                marked_pr = True
+
+            for review in gprr_pr.reviews:
+                if self.accounts_filter.contains_item_id(review.user.id):
+                    self.by_reviewer.append_item(gprr_pr, review.user.name)
+                    marked_pr = True
+
+            for assignee in gprr_pr.assignees:
+                if self.accounts_filter.contains_item_id(assignee.id):
+                    self.by_reviewer.append_item(gprr_pr, assignee.name)
+                    marked_pr = True
+
+            if marked_pr:
+                self.by_repository.append_item(gprr_pr, repo.name)
+                self.full_list.append_item(gprr_pr)
+
+    def __convert_pr(
             self,
-            filename: str = __default_json_report_name
-    ):
-        pass
+            pr: PullRequest,
+    ) -> GprrPR:
+        gprr_pr = GprrPR()
+        gprr_pr.id = pr.id
+        gprr_pr.number = pr.number
+        gprr_pr.url = pr.html_url
+        gprr_pr.repository = pr.head.repo.name
+        gprr_pr.title = pr.title
+        gprr_pr.creator = self.__convert_user(pr.user)
+        gprr_pr.created = pr.created_at
+        gprr_pr.updated = pr.updated_at
+        gprr_pr.since_updated = datetime.datetime.today() - gprr_pr.updated
+
+        for assignee in pr.assignees:
+            gprr_pr.assignees.append(self.__convert_user(assignee))
+
+        gprr_pr.flags.append(GprrPrFlag("Draft", str(pr.draft)))
+        gprr_pr.flags.append(GprrPrFlag("Mergeable", str(pr.mergeable)))
+        gprr_pr.flags.append(GprrPrFlag("Mergeable State", str(pr.mergeable_state)))
+
+        for label in pr.get_labels():
+            gprr_pr.labels.append(self.__convert_label(label))
+        gprr_pr.initial_branch = pr.base.ref
+
+        active_reviewers = []
+        for review in pr.get_reviews():
+            reviewer = self.__convert_user(review.user)
+            if not self.__known_review(active_reviewers, reviewer):
+                active_reviewers.append(reviewer)
+                gprr_review = GprrReview(
+                    user=reviewer,
+                    state=review.state,
+                    submitted_at=review.submitted_at,
+                    url=review.html_url,
+                )
+                gprr_pr.reviews.append(gprr_review)
+        for revusr in pr.get_review_requests()[0]:
+            usr = self.__convert_user(revusr)
+            review = GprrReview(user=usr, state="PENDING")
+            gprr_pr.reviews.append(review)
+
+    def __known_review(
+            self,
+            active_reviewers: List[GprrUser],
+            user: GprrUser
+    ) -> bool:
+        for rvw in active_reviewers:
+            if rvw.id == user.id:
+                return True
+        return False
+
+    def __convert_label(
+            self,
+            label: Label
+    ) -> GprrPrLabel:
+        return GprrPrLabel(
+            title=label.name,
+            color=label.color,
+            description=label.description
+        )
+
+    def __convert_user(
+            self,
+            user: NamedUser
+    ) -> GprrUser:
+        usr = GprrUser(
+            login=user.login,
+            name=user.name,
+            id=user.id,
+            url=user.html_url
+        )
+        return usr
+
+    def __convert_repo(self, repo: Repository) -> GprrRepository:
+        assert repo is not None
+        return GprrRepository(
+            id=repo.id,
+            title=repo.description,
+            url=repo.html_url,
+            name=repo.name
+        )
+
+
+    # def generate_html_report(
+    #         self,
+    #         filename: str = __default_html_report_name
+    # ):
+    #     pass
+    #
+    # def generate_json_report(
+    #         self,
+    #         filename: str = __default_json_report_name
+    # ):
+    #     pass
+
